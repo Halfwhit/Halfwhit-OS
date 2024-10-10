0297b090c421c5dba814de1dadf1a081b0b21b9a
Halfwhit <Halfwhit@gmail.com>
2024-10-10 16:13:26 +0100

    Update config.py

--- configs/qtile/config.py
+++ configs/qtile/config.py
@@ -176,7 +176,6 @@ screens = [
                     },
                     name_transform=lambda name: name.upper(),
                 ),
-                widget.TextBox("Press &lt;M-r&gt; to spawn", foreground="#d75f5f"),
                 # NB Systray is incompatible with Wayland, consider using StatusNotifier instead
                 # widget.StatusNotifier(),
                 widget.Systray(),
