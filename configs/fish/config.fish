fbf0f3636badd7d13d0c6de9c2703f8a53d3bbcc
Halfwhit <Halfwhit@gmail.com>
2024-10-10 15:57:18 +0100

    Update config.fish with Zellij settings

--- configs/fish/config.fish
+++ configs/fish/config.fish
@@ -1,4 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    set ZELLIJ_AUTO_FOCUS true
+    set ZELLIJ_AUTO_EXIT true
+    eval (zellij setup --generate-auto-start fish | string collect)
 end
 starship init fish | source
